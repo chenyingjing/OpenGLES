2d84a8d397603d4b59a68060cfad2631d4f82192
chenyingjing <dcba63@gmail.com>
2016-11-02 21:13:39 +0800

    OpenGL ES Tutorial 01

---
+++ OpenGLESTutorial01/Tutorial01/Tutorial01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tutorial01
+//
+//  Created by aa64mac on 01/11/2016.
+//  Copyright © 2016 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLESTutorial01/Tutorial01/Tutorial01/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tutorial01
+//
+//  Created by aa64mac on 01/11/2016.
+//  Copyright © 2016 cyj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
