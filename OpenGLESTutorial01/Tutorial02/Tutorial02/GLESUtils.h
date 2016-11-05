e48394b1c657c5dd12340ed67a7185b91ce3a4b1
chenyingjing <dcba63@gmail.com>
2016-11-05 12:48:30 +0800

    Tutorial02

---
+++ OpenGLESTutorial01/Tutorial02/Tutorial02/GLESUtils.h
@@ -0,0 +1,20 @@
+//
+//  GLESUtils.h
+//  Tutorial02
+//
+//  Created by aa64mac on 05/11/2016.
+//  Copyright © 2016 cyj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include <OpenGLES/ES2/gl.h>
+
+@interface GLESUtils : NSObject
+
+// Create a shader object, load the shader source string, and compile the shader.
+//
++(GLuint)loadShader:(GLenum)type withString:(NSString *)shaderString;
+
++(GLuint)loadShader:(GLenum)type withFilepath:(NSString *)shaderFilepath;
+
+@end
---
+++ OpenGLESTutorial01/Tutorial02/Tutorial02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tutorial02
+//
+//  Created by aa64mac on 05/11/2016.
+//  Copyright © 2016 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
