d76836b11323430607d7ffdcedb52cf859dcf532
chenyingjing <dcba63@gmail.com>
2016-11-20 12:59:04 +0800

    Correct the algorithm of specular.

--- OpenGLESTutorial01/Tutorial071/Tutorial071/Shader/VertexShader.glsl
+++ OpenGLESTutorial01/Tutorial071/Tutorial071/Shader/VertexShader.glsl
@@ -13,6 +13,7 @@ attribute vec4 vDiffuseMaterial;
 
 varying vec4 vDestinationColor;
 
+/*
 void main(void)
 {
     gl_Position = projection * modelView * vPosition;
@@ -29,4 +30,30 @@ void main(void)
     vDestinationColor = vAmbientMaterial + df * vDiffuseMaterial + sf * vSpecularMaterial;
     
     //vDestinationColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+*/
+
+void main(void)
+{
+    gl_Position = projection * modelView * vPosition;
+    
+    vec3 N = normalMatrix * vNormal;
+    vec3 L = normalize(vLightPosition);
+    vec3 E = vec3(0, 0, 1);
+    
+    
+    vec3 surfacePos = vec3(modelView * vPosition);
+    vec3 surfaceToLight = normalize(vLightPosition - surfacePos);
+    vec3 incidenceVector = -surfaceToLight; //a unit vector
+    vec3 reflectionVector = reflect(incidenceVector, N); //also a unit vector
+    
+    vec3 surfaceToCamera = normalize(E - surfacePos); //also a unit
+    float cosAngle = max(0.0, dot(surfaceToCamera, reflectionVector));
+    float df = max(0.0, dot(N, L));
+    float sf = 0.0;
+    if (cosAngle > 0.0) {
+        sf = pow(cosAngle, shininess);
+    } 
+    
+    vDestinationColor = vAmbientMaterial + df * vDiffuseMaterial + sf * vSpecularMaterial;
 }
