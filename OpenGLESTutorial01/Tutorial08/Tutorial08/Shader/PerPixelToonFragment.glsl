eb63a3d0dd88454bc49863746aea29c27604e0b3
chenyingjing <dcba63@gmail.com>
2016-11-26 18:27:11 +0800

    Change the specular algorithm in PerPixelToonFragment.

--- OpenGLESTutorial01/Tutorial08/Tutorial08/Shader/PerPixelToonFragment.glsl
+++ OpenGLESTutorial01/Tutorial08/Tutorial08/Shader/PerPixelToonFragment.glsl
@@ -2,6 +2,7 @@
 
 varying mediump vec3 vEyeSpaceNormal;
 varying mediump vec4 vDiffuse;
+varying mediump vec4 vPosInWorld;
 
 uniform highp vec3 vLightPosition;
 uniform highp vec4 vAmbientMaterial;
@@ -9,16 +10,21 @@ uniform highp vec4 vSpecularMaterial;
 uniform highp float shininess;
 
 
-void main()
+void main(void)
 {
     highp vec3 N = normalize(vEyeSpaceNormal);
     highp vec3 L = normalize(vLightPosition);
     highp vec3 E = vec3(0, 0, 1);
-    highp vec3 H = normalize(L + E);
     
+    
+    highp vec3 surfacePos = vec3(vPosInWorld);
+    highp vec3 surfaceToLight = normalize(vLightPosition - surfacePos);
+    highp vec3 incidenceVector = -surfaceToLight; //a unit vector
+    highp vec3 reflectionVector = reflect(incidenceVector, N); //also a unit vector
+    
+    highp vec3 surfaceToCamera = normalize(E - surfacePos); //also a unit
+    highp float cosAngle = max(0.0, dot(surfaceToCamera, reflectionVector));
     highp float df = max(0.0, dot(N, L));
-    highp float sf = max(0.0, dot(N, H));
-    sf = pow(sf, shininess);
     
     if (df < 0.1)
         df = 0.0;
@@ -33,7 +39,11 @@ void main()
     else
         df = 1.0;
     
+    highp float sf = 0.0;
+    if (cosAngle > 0.0) {
+        sf = pow(cosAngle, shininess);
+    }
+    
     gl_FragColor = vAmbientMaterial + df * vDiffuse + sf * vSpecularMaterial;
-
 }
 
