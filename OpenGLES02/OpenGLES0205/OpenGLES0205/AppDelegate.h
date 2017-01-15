6e26a7a7bf66c600822888a95990dd2758a448a8
chenyingjing <dcba63@gmail.com>
2017-01-15 16:59:44 +0800

    start OpenGLES0205

---
+++ OpenGLES02/OpenGLES0205/OpenGLES0205/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLES0205
+//
+//  Created by aa64mac on 15/01/2017.
+//  Copyright © 2017 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLES02/OpenGLES0205/OpenGLES0205/OpenGLView.mm
@@ -0,0 +1,374 @@
+//
+//  OpenGLView.m
+//  Tutorial01
+//
+//  Created by aa64mac on 01/11/2016.
+//  Copyright © 2016 cyj. All rights reserved.
+//
+
+#import "OpenGLView.h"
+
+#include "tdogl/Program.h"
+#include "ResourcePath/ResourcePath.hpp"
+#include "tdogl/Texture.h"
+#include "common/thirdparty/stb_image/stb_image.h"
+#define GLM_FORCE_RADIANS
+#include <glm/gtc/matrix_transform.hpp>
+#include "tdogl/Camera.h"
+
+@interface OpenGLView() {
+    tdogl::Program* gProgram;
+    GLuint gVAO;
+    GLuint gVBO;
+    tdogl::Texture* gTexture;
+    //GLuint gTex;
+    GLfloat gDegreesRotated;
+    CADisplayLink * _displayLink;
+    tdogl::Camera gCamera;
+}
+
+- (void)setupLayer;
+- (void)setupContext;
+- (void)setupRenderBuffer;
+- (void)setupFrameBuffer;
+- (void)destoryRenderAndFrameBuffer;
+
+@end
+
+@implementation OpenGLView
+
++ (Class)layerClass {
+    // 只有 [CAEAGLLayer class] 类型的 layer 才支持在其上描绘 OpenGL 内容。
+    return [CAEAGLLayer class];
+}
+
+- (void)setupLayer
+{
+    _eaglLayer = (CAEAGLLayer*) self.layer;
+    
+    // CALayer 默认是透明的，必须将它设为不透明才能让其可见
+    _eaglLayer.opaque = YES;
+    
+    // 设置描绘属性，在这里设置不维持渲染内容以及颜色格式为 RGBA8
+    _eaglLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:
+                                     [NSNumber numberWithBool:NO], kEAGLDrawablePropertyRetainedBacking, kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat, nil];
+}
+
+- (void)setupContext {
+    // 指定 OpenGL 渲染 API 的版本，在这里我们使用 OpenGL ES 2.0
+    EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
+    _context = [[EAGLContext alloc] initWithAPI:api];
+    if (!_context) {
+        NSLog(@"Failed to initialize OpenGLES 2.0 context");
+        exit(1);
+    }
+    
+    // 设置为当前上下文
+    if (![EAGLContext setCurrentContext:_context]) {
+        NSLog(@"Failed to set current OpenGL context");
+        exit(1);
+    }
+}
+
+- (void)setupRenderBuffer
+{
+    glGenRenderbuffers(1, &_colorRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+    // 为 color renderbuffer 分配存储空间
+    [_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
+}
+
+- (void)setupFrameBuffer
+{
+    glGenFramebuffers(1, &_frameBuffer);
+    // 设置为当前 framebuffer
+    glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer);
+    // 将 _colorRenderBuffer 装配到 GL_COLOR_ATTACHMENT0 这个装配点上
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                              GL_RENDERBUFFER, _colorRenderBuffer);
+}
+
+- (void)setupDepthBuffer
+{
+    int width, height;
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &width);
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &height);
+    
+    glGenRenderbuffers(1, &_depthRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _depthRenderBuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
+    
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+                              GL_RENDERBUFFER, _depthRenderBuffer);
+    
+    glBindRenderbuffer(GL_RENDERBUFFER, _colorRenderBuffer);
+
+}
+
+- (void)destoryRenderAndFrameBuffer
+{
+    if (_colorRenderBuffer != 0) {
+        glDeleteRenderbuffers(1, &_colorRenderBuffer);
+        _colorRenderBuffer = 0;
+    }
+    
+    if (_frameBuffer != 0) {
+        glDeleteFramebuffers(1, &_frameBuffer);
+        _frameBuffer = 0;
+    }
+    
+    if (_depthRenderBuffer != 0) {
+        glDeleteFramebuffers(1, &_depthRenderBuffer);
+        _depthRenderBuffer = 0;
+    }
+}
+
+- (void)layoutSubviews {
+    
+    [self setupLayer];
+    
+    [self setupContext];
+    
+    [self InitGL];
+    
+    [self destoryRenderAndFrameBuffer];
+    
+    [self setupRenderBuffer];
+    
+    [self setupFrameBuffer];
+    
+    [self setupDepthBuffer];
+    
+    [self LoadTexture];
+    
+    [self LoadShaders];
+
+    [self LoadTriangle];
+    
+    [self Render];
+}
+
+- (void)InitGL
+{
+    NSLog(@"OpenGL version: %s", glGetString(GL_VERSION));
+    NSLog(@"GLSL version: %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
+    NSLog(@"Vendor: %s", glGetString(GL_VENDOR));
+    NSLog(@"Renderer: %s", glGetString(GL_RENDERER));
+    
+    glEnable(GL_DEPTH_TEST);
+    //glDepthFunc(GL_LESS);
+    //glDepthFunc(GL_GREATER);
+//    glEnable(GL_BLEND);
+//    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    gCamera.setPosition(glm::vec3(0, 0, 10));
+    gCamera.setViewportAspectRatio(self.frame.size.width / self.frame.size.height);
+    //gCamera.setNearAndFarPlanes(0.1, 5000);
+}
+
+- (void)LoadTexture
+{
+    tdogl::Bitmap bmp = tdogl::Bitmap::bitmapFromFile(ResourcePath("wooden-crate", "jpg"));
+    bmp.flipVertically();
+    gTexture = new tdogl::Texture(bmp);
+}
+
+- (void)LoadShaders
+{
+    std::vector<tdogl::Shader> shaders;
+    shaders.push_back(tdogl::Shader::shaderFromFile(ResourcePath("VertexShader", "glsl"), GL_VERTEX_SHADER));
+    shaders.push_back(tdogl::Shader::shaderFromFile(ResourcePath("FragmentShader", "glsl"), GL_FRAGMENT_SHADER));
+    gProgram = new tdogl::Program(shaders);
+}
+
+- (void)LoadTriangle
+{
+    // make and bind the VAO
+    glGenVertexArraysOES(1, &gVAO);
+    glBindVertexArrayOES(gVAO);
+    
+    // make and bind the VBO
+    glGenBuffers(1, &gVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, gVBO);
+    
+    // Put the three triangle verticies into the VBO
+    GLfloat vertexData[] = {
+        //  X     Y     Z       U     V
+        // bottom
+        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+        1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+        1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+        1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+        
+        // top
+        -1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+        -1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+        1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+        1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+        -1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+        
+        // front
+        -1.0f,-1.0f, 1.0f,   1.0f, 0.0f,
+        1.0f,-1.0f, 1.0f,   0.0f, 0.0f,
+        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,   0.0f, 0.0f,
+        1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+        
+        // back
+        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+        -1.0f, 1.0f,-1.0f,   0.0f, 1.0f,
+        1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+        1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+        -1.0f, 1.0f,-1.0f,   0.0f, 1.0f,
+        1.0f, 1.0f,-1.0f,   1.0f, 1.0f,
+        
+        // left
+        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+        -1.0f,-1.0f,-1.0f,   0.0f, 0.0f,
+        -1.0f,-1.0f, 1.0f,   0.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,   1.0f, 0.0f,
+        
+        // right
+        1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+        1.0f,-1.0f,-1.0f,   1.0f, 0.0f,
+        1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+        1.0f,-1.0f, 1.0f,   1.0f, 1.0f,
+        1.0f, 1.0f,-1.0f,   0.0f, 0.0f,
+        1.0f, 1.0f, 1.0f,   0.0f, 1.0f
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+
+    
+    // connect the xyz to the "vert" attribute of the vertex shader
+    glEnableVertexAttribArray(gProgram->attrib("vert"));
+    glVertexAttribPointer(gProgram->attrib("vert"), 3, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), NULL);
+    
+    glEnableVertexAttribArray(gProgram->attrib("vertTexCoord"));
+    glVertexAttribPointer(gProgram->attrib("vertTexCoord"), 2, GL_FLOAT, GL_TRUE,  5*sizeof(GLfloat), (const GLvoid*)(3 * sizeof(GLfloat)));
+
+    // unbind the VBO and VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArrayOES(0);
+    
+}
+
+- (void)Render
+{
+    // clear everything
+    glClearColor(0, 0, 0, 1); // black
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    
+    // bind the program (the shaders)
+    glUseProgram(gProgram->object());
+    
+    gProgram->setUniform("camera", gCamera.matrix());
+    
+    float scale = 0.8;
+    glm::mat4 _model = glm::scale(glm::mat4(), glm::vec3(scale, scale, scale));
+    gProgram->setUniform("model", glm::rotate(_model, glm::radians(gDegreesRotated), glm::vec3(0,1,0)));
+    //NSLog(@"gDegreesRotated:%f", gDegreesRotated);
+    //gProgram->setUniform("model", _model);
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, gTexture->object());
+    //glBindTexture(GL_TEXTURE_2D, gTex);
+//    gProgram->setUniform("tex", 0);
+    GLint samplerSlot = glGetUniformLocation(gProgram->object(), "tex");
+    glUniform1i(samplerSlot, 0);
+    
+    // bind the VAO (the triangle)
+    glBindVertexArrayOES(gVAO);
+    
+    // draw the VAO
+    glDrawArrays(GL_TRIANGLES, 0, 6*2*3);
+    
+    // unbind the VAO
+    glBindVertexArrayOES(0);
+    
+    // unbind the program
+    glUseProgram(0);
+    
+    // swap the display buffers (displays what was just drawn)
+    [_context presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+- (void)Update: (float)secondsElapsed
+{
+    const GLfloat degreesPerSecond = 180.0f;
+    gDegreesRotated += secondsElapsed * degreesPerSecond;
+    
+    //don't go over 360 degrees
+    while(gDegreesRotated > 360.0f) gDegreesRotated -= 360.0f;
+
+    //move position of camera based on WASD keys
+    const float moveSpeed = 2.0; //units per second
+    if (self.backwardButton.highlighted){
+        gCamera.offsetPosition(secondsElapsed * moveSpeed * -gCamera.forward());
+    } else if (self.forwardButton.highlighted){
+        gCamera.offsetPosition(secondsElapsed * moveSpeed * gCamera.forward());
+    }
+    if (self.leftButton.highlighted){
+        gCamera.offsetPosition(secondsElapsed * moveSpeed * -gCamera.right());
+    } else if (self.rightButton.highlighted){
+        gCamera.offsetPosition(secondsElapsed * moveSpeed * gCamera.right());
+    }
+    if (self.downButton.highlighted){
+        gCamera.offsetPosition(secondsElapsed * moveSpeed * -glm::vec3(0,1,0));
+    } else if (self.upButton.highlighted){
+        gCamera.offsetPosition(secondsElapsed * moveSpeed * glm::vec3(0,1,0));
+    }
+    
+    const float mouseSensitivity = 0.1f;
+    gCamera.offsetOrientation(mouseSensitivity * self.moveY, mouseSensitivity * self.moveX);
+    
+    float fieldOfView = 50;
+    fieldOfView *= self.scale;
+    if(fieldOfView < 5.0f) fieldOfView = 5.0f;
+    if(fieldOfView > 130.0f) fieldOfView = 130.0f;
+    gCamera.setFieldOfView(fieldOfView);
+
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+
+        
+        [self toggleDisplayLink];
+        
+    }
+    
+    return self;
+}
+
+- (void)toggleDisplayLink
+{
+    if (_displayLink == nil) {
+        _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(displayLinkCallback:)];
+        [_displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    }
+    else {
+        [_displayLink invalidate];
+        [_displayLink removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+        _displayLink = nil;
+    }
+}
+
+- (void)displayLinkCallback:(CADisplayLink*)displayLink
+{
+    [self Update:displayLink.duration];
+    
+    [self Render];
+}
+
+@end
