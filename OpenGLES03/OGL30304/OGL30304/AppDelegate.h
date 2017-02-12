b579f4597153391eb5d1eb698ab0b22be61591d3
chenyingjing <dcba63@gmail.com>
2017-02-12 12:09:56 +0800

    Get started OGL30304.

---
+++ OpenGLES03/OGL30304/OGL30304/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OGL30304
+//
+//  Created by aa64mac on 12/02/2017.
+//  Copyright © 2017 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
