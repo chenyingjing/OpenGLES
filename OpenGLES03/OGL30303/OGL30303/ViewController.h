0850b9f714a48d86cbd7c228e6d9c413f3c735e4
chenyingjing <dcba63@gmail.com>
2017-02-12 11:08:39 +0800

    Get started OGL30303.

---
+++ OpenGLES03/OGL30303/OGL30303/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  OpenGLES0201
+//
+//  Created by aa64mac on 02/01/2017.
+//  Copyright © 2017 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OpenGLView.h"
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet OpenGLView *openGLView;
+
+@end
+
