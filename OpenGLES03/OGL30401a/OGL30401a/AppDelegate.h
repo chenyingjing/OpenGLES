118e34039c32d74c11fcf0e68cb3bcd9ba34f584
chenyingjing <dcba63@gmail.com>
2017-02-27 21:19:04 +0800

    Get started OGL30401a.

---
+++ OpenGLES03/OGL30401a/OGL30401a/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OGL30401a
+//
+//  Created by aa64mac on 27/02/2017.
+//  Copyright © 2017 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
