426e6f6498ce982d6d011ab6c7cc5537961abbc2
chenyingjing <dcba63@gmail.com>
2017-03-04 18:11:58 +0800

    Get started OGL30404a.

--- OpenGLES03/OGL30404a/OGL30404a/AppDelegate.m
+++ OpenGLES03/OGL30404a/OGL30404a/AppDelegate.m
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.m
-//  OGL30403a
+//  OGL30404a
 //
 //  Created by aa64mac on 04/03/2017.
 //  Copyright © 2017 cyj. All rights reserved.
@@ -47,9 +47,5 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
-- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
-{
-    return UIInterfaceOrientationMaskPortrait;
-}
 
 @end
