39127d3ca028e92da7b8ffac1e53c87c7763eda7
chenyingjing <dcba63@gmail.com>
2017-03-11 17:23:13 +0800

    Draw tree.

--- OpenGLES03/OGL304Tree/OGL304Tree/OpenGLView.mm
+++ OpenGLES03/OGL304Tree/OGL304Tree/OpenGLView.mm
@@ -23,7 +23,7 @@ OBJMESH *objmesh = NULL;
 
 extern void (*programBindAttribLocation)(GLuint);
 
-#define OBJ_FILE (char *)"ball.obj"
+#define OBJ_FILE (char *)"tree.obj"
 
 struct ModelAsset {
     tdogl::Program* shaders;
@@ -178,14 +178,14 @@ struct ModelInstance {
     glEnable( GL_DEPTH_TEST );
     glEnable( GL_CULL_FACE  );
     
-//    projection = glm::rotate(glm::mat4(), glm::radians(-90.0f), glm::vec3(0,0,1));
+    projection = glm::rotate(glm::mat4(), glm::radians(-90.0f), glm::vec3(0,0,1));
 //    projection = glm::rotate(projection, glm::radians(-90.0f), glm::vec3(1,0,0));
 //    projection = glm::mat4();
     _model = glm::mat4();
     
     gCamera.setFieldOfView(45.0f);
     gCamera.lookAt(glm::vec3(0, 0, 0));
-    gCamera.setPosition(glm::vec3(0, 0, 3));
+    gCamera.setPosition(glm::vec3(2, 0, 7));
     gCamera.setViewportAspectRatio(self.frame.size.width / self.frame.size.height);
     gCamera.setNearAndFarPlanes(0.1, 5000);
 }
@@ -386,7 +386,7 @@ void program_bind_attrib_location(GLuint pid) {
 //                           &position,
 //                           &gfx.modelview_matrix[ gfx.modelview_matrix_index - 1 ] );
 
-        vec3 lightPosition    = { 0.0f, 2.0f, 2.0f };
+        vec3 lightPosition    = { 0.0f, 4.0f, 2.0f };
         glUniform3fv( lightPositionSlot,
                      1,
                      (float *)&lightPosition );
