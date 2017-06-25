ac8f3c8cc5acc5333013622677a7f510a7f0fe24
chenyingjing <dcba63@gmail.com>
2017-06-25 10:49:43 +0800

    Draw a triangle with opengl es 3

---
+++ OpenGLES3/sx1989827/t02/t02/t02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  t02
+//
+//  Created by aa64mac on 25/06/2017.
+//  Copyright © 2017 cyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
